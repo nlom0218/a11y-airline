3fb8f343a4fe5f38b5d70c3288308b8cd1e62f2b
nlom0218 <nlom0218@naver.com>
2023-07-28 12:30:06 +0900

    feat: 증감 상태 메시지 출력

--- src/components/SpinButton.tsx
+++ src/components/SpinButton.tsx
@@ -1,21 +1,39 @@
-import React, { useState, MouseEvent } from "react";
-import "./SpinButton.css";
+import React, { useState, MouseEvent, useEffect } from 'react';
+import './SpinButton.css';
 
 const SpinButton: React.FC = () => {
   const [count, setCount] = useState<number>(0);
   const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false);
+  const [text, setText] = useState<null | string>(null);
 
   const increment = () => {
+    if (count > 2) {
+      setText('최대 인원수는 3명까지 가능합니다');
+      return;
+    }
+    setText(`성인 승객 추가 ${count + 1}`);
     setCount((prevCount) => prevCount + 1);
   };
 
   const decrement = () => {
+    if (count < 1) {
+      setText('최소 인원수는 0명까지 가능합니다');
+      return;
+    }
+    setIsTooltipVisible(false);
+    setText(`성인 승객 감소 ${count - 1}`);
     setCount((prevCount) => prevCount - 1);
   };
 
   const toggleTooltip = (event: MouseEvent<HTMLDivElement>) => {
     setIsTooltipVisible(!isTooltipVisible);
   };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setText(null);
+    }, 1000);
+  }, [text]);
 
   return (
     <section className="spinButtonContainer">
@@ -34,7 +52,11 @@ const SpinButton: React.FC = () => {
             )}
           </div>
         </div>
-        <button onClick={decrement} className="spinButton">
+        <button
+          onClick={decrement}
+          className="spinButton"
+          aria-label="성인 탑승자 한명 줄이기"
+        >
           -
         </button>
         <input
@@ -43,10 +65,21 @@ const SpinButton: React.FC = () => {
           readOnly
           className="spinButtonInput"
           value={count}
+          aria-label="성인"
         />
-        <button onClick={increment} className="spinButton">
+        <button
+          onClick={increment}
+          className="spinButton"
+          aria-label="성인 탑승자 한명 늘리기"
+        >
           +
         </button>
+        <div
+          role="status"
+          style={{ position: 'absolute', top: '1000px', left: '1000px' }}
+        >
+          {text}
+        </div>
       </div>
     </section>
   );
